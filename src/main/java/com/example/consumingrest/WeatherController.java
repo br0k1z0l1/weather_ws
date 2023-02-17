2ff47f0da9963ac1602ef99976c9f67ffcab58ad
T.Sanyi <s.truczkai@gmail.com>
2023-02-17 10:19:28 +0100

    add SimpleWeatherResponse

--- src/main/java/com/example/consumingrest/WeatherController.java
+++ src/main/java/com/example/consumingrest/WeatherController.java
@@ -1,5 +1,6 @@
 package com.example.consumingrest;
 
+import com.example.consumingrest.responsemodel.SimpleWeatherResponse;
 import com.example.consumingrest.responsemodel.openweathermap.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,8 +20,9 @@ public class WeatherController {
     }
 
     @GetMapping("/api/weather")
-    public WeatherResponse getWeather() {
-        return restTemplate.getForObject(
+    public SimpleWeatherResponse getWeather() {
+        WeatherResponse weatherResponse = restTemplate.getForObject(
                 "https://api.openweathermap.org/data/2.5/weather?q=Budapest,HU&appid=" + apiKey + "&units=metric&lang=hu", WeatherResponse.class);
+    return new SimpleWeatherResponse(weatherResponse);
     }
 }
